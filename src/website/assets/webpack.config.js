74b47b6d976a1fa38fab1714e37fe7154b2d0b95
Jean de Klerk <jadekler@gmail.com>
2016-10-10 21:09:15 -0600

    Formattingg

--- src/website/assets/webpack.config.js
+++ src/website/assets/webpack.config.js
@@ -5,44 +5,44 @@ var APP_DIR = path.resolve(__dirname, 'src');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
-    entry: APP_DIR + '/app.jsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
+  entry: APP_DIR + '/app.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
 
-                // Skip any files outside of your project's `src` directory
-                include: [
-                    path.resolve(__dirname, 'src'),
-                ],
+        // Skip any files outside of your project's `src` directory
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
 
-                // Only run `.js` and `.jsx` files through Babel
-                test: /\.jsx?$/,
+        // Only run `.js` and `.jsx` files through Babel
+        test: /\.jsx?$/,
 
-                // Options to configure babel with
-                query: {
-                    presets: ['es2015', 'react'],
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('css!sass')
-            }
-        ]
-    },
-    sassLoader: {
-        includePaths: [path.resolve(__dirname, './stylesheets')]
-    },
-    plugins: [
-        new ExtractTextPlugin('app.css')
+        // Options to configure babel with
+        query: {
+          presets: ['es2015', 'react'],
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
     ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './stylesheets')]
+  },
+  plugins: [
+    new ExtractTextPlugin('app.css')
+  ]
 };
 
 module.exports = config;
